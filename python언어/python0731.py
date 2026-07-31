343f4fd8dbca43f8e90d9882b493bbe7cebccf13
najongjine <najongjin3@hotmail.com>
2026-07-31 16:48:16 +0900

    .

--- python언어/python0731.py
+++ python언어/python0731.py
@@ -17,5 +17,26 @@ str1="1211"
 """
 input 함수를 통해 111을 받았다고 가정하자
 """
-a=int(input()) # c 언어의 scanf 와 비슷
-print(f"a의 타입:{type(a)}")
+#a=int(input()) # c 언어의 scanf 와 비슷
+#print(f"a의 타입:{type(a)}")
+
+
+print('2026', '07', '31', sep='-') #2026-07-31
+# 자동개행 없어지고 끝에 ... 나옴
+print("문장1",end="...")
+print("문장2") # 문장1...문장2
+
+a=1
+b=2
+"""
+논리 비트연산 있음
+print(a^b)
+print(a&b)
+print(a|b)
+"""
+
+0<=a<=100
+
+
+
+
