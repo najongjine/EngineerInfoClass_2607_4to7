d92f615b0033602cc8c2965ed6f968a56c242cb2
najongjine <najongjin3@hotmail.com>
2026-07-31 16:35:29 +0900

    .

---
+++ python언어/python0731.py
@@ -0,0 +1,21 @@
+num1=1
+f1=1.2
+b1=False
+str1="ab"
+print(f"str1의 타입:{type(str1)}")
+str1=1
+"""
+print(f"num1의 타입:{type(num1)}")
+print(f"f1의 타입:{type(f1)}")
+print(f"b1의 타입:{type(b1)}")
+print(f"str1의 타입:{type(str1)}")
+"""
+
+
+str1="1211"
+
+"""
+input 함수를 통해 111을 받았다고 가정하자
+"""
+a=int(input()) # c 언어의 scanf 와 비슷
+print(f"a의 타입:{type(a)}")
