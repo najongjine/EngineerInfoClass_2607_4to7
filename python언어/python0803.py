e9f642480b9bca86b2cf2dff098c5ea5790dc4ad
najongjine <najongjin3@hotmail.com>
2026-08-03 16:46:48 +0900

    .

--- python언어/python0803.py
+++ python언어/python0803.py
@@ -37,6 +37,23 @@ str1[::2] # HloPto
 
 
 arr1=['h','e','l','l','o',' ','P','e']
+
+"""
+.index:= 왼쪽부터 오른쪽으로 검색하면서 'l' 이라는거 찾고,
+그곳의 엔덱스 번호를 return
+"""
+idx_num=arr1.index('l') # 2
+
+"""
+python에서 pop()은 stack 개념이랑 비슷하면서도 룰을 어길수도 있어요
+pop() 이렇게 하면 리스트에서 맨 마지막꺼 뿅 꺼내와요
+pop(2) 이렇게하면 리스트에서 인덱스 2번째의 원소 뿅 꺼내와요
+그리고 중요한건 pop()을 하고나면 진짜 원본 리스트에서도 원소가 제거되요
+"""
+# mye=arr1.pop() # e
+mye=arr1.pop(2) # l
+print(f"mye:",mye)
+print(f"arr1:",arr1)
+
 # remove:= 하나만 찾아서 제거. 왼쪽부터 오른쪽으로 검색해 감
 arr1.remove('e') # ['h', 'l', 'l', 'o', ' ', 'P', 'e']
-print(arr1)
