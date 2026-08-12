2cf0b714f6c988250e3b86899df13677ed4e11fa
najongjine <najongjin3@hotmail.com>
2026-08-12 16:34:59 +0900

    .

--- python언어/dummy.py
+++ python언어/dummy.py
@@ -1,5 +1,8 @@
-a="abc"
-b=a
-b[0]='p'
-print(f"a:{a}")
-print(f"b:{b}")
+a = ['한국', '중국', '일본',"일본"]
+a.append('베트남')
+a.extend(["한국","일본","중국","호주"])
+a.remove('일본')
+a=set(a)
+a.update({'한국'})
+print(a)
+
