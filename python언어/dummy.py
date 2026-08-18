bd25ea2d539d3e850b5b38263b7c7ca635e30ef0
najongjine <najongjin3@hotmail.com>
2026-08-18 16:37:14 +0900

    .

--- python언어/dummy.py
+++ python언어/dummy.py
@@ -1,8 +1,6 @@
-a = ['한국', '중국', '일본',"일본"]
-a.append('베트남')
-a.extend(["한국","일본","중국","호주"])
-a.remove('일본')
-a=set(a)
-a.update({'한국'})
-print(a)
-
+a = "eng in eer"
+b = a[:3]
+c = a[4:6]
+d = a[8:]
+e=b+c+d
+print(e)
